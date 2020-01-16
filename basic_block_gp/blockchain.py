99b63f514da61fe079dbe67e191b0793ba21a77f
Neha Patel <neha.r.p@gmail.com>
2020-01-15 16:31:01 -0800

    transferred blockchain.py code to client_mining folder

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -90,7 +90,7 @@ class Blockchain(object):
         in an effort to find a number that is a valid proof
         :return: A valid proof for the provided block
         """
-        block_string = json.dumps(block).encode()
+        block_string = json.dumps(block)
         proof = 0
         while self.valid_proof(block_string, proof) is False:
             proof += 1
@@ -108,7 +108,7 @@ class Blockchain(object):
         correct number of leading zeroes.
         :return: True if the resulting hash is a valid proof, False otherwise
         """
-        guess = f"{block_string}{proof}"
+        guess = f"{block_string}{proof}".encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
 
         return guess_hash[:3] == "000"
@@ -128,11 +128,14 @@ blockchain = Blockchain()
 @app.route('/mine', methods=['GET'])
 def mine():
     # Run the proof of work algorithm to get the next proof
-
+    proof = blockchain.proof_of_work(blockchain.last_block)
     # Forge the new Block by adding it to the chain with the proof
+    previous_hash = blockchain.hash(blockchain.last_block)
+    block = blockchain.new_block(proof, previous_hash)
 
     response = {
-        # TODO: Send a JSON response with the new block
+        # Send a JSON response with the new block
+        'new_block': block
     }
 
     return jsonify(response), 200
